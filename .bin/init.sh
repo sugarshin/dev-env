4f6e0bad8a511c50d92e2b7d26ddde2f461de93c
sugarshin <shinsugar@gmail.com>
2015-03-08 16:07:22 +0900

    fix

--- .bin/init.sh
+++ .bin/init.sh
@@ -2,5 +2,5 @@
 
 npm i -D browserify coffeeify jade live-server stylus watchify
 mkdir -p src/{html,css,js} build/{css,js}
-touch src/html/index.html src/css/index.styl src/js/main.coffee
+touch src/html/index.jade src/css/index.styl src/js/main.coffee
 rm -rf .git
